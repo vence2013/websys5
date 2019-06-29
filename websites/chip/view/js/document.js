e7155ed6bb0fbe185b4c7de324eaf10ba45743fb
wuxb <vence2013@163.com>
2019-06-29 13:00:34 +0800

    完善寄存器和位组的详细信息显示，添加寄存器和位组搜索

--- websites/chip/view/js/document.js
+++ websites/chip/view/js/document.js
@@ -18,6 +18,11 @@ function documentCtrl($scope, $http, $interval, user)
     $scope.chip = null;
     $scope.module = null;
     $scope.bitindex = []; // 位映射的索引
+    // 寄存器和位组搜索
+    $scope.str = '';
+
+    $scope.$watch('str', mapSearch);
+
 
     var docid   = $('#wrapper').attr('docid');
     $scope.docid= /^\d+$/.test(docid) ? parseInt(docid) : 0;    
@@ -32,7 +37,7 @@ function documentCtrl($scope, $http, $interval, user)
     var editor = editormd("editormd", {
         path : '/node_modules/editor.md/lib/',
         width: '100%',
-        height: 200,
+        height: 220,
         toolbarIcons : function() {
             return editormd.toolbarModes['simple']; // full, simple, mini
         },    
@@ -117,7 +122,12 @@ function documentCtrl($scope, $http, $interval, user)
 
             var ret = res.data.message;            
             var desc = ret.desc.replace(new RegExp('\n',"gm"),'<br/>');
-            $('#bitsInfo').html(ret.fullname+'<br/>'+desc).css('display', 'block');
+            var title = "<span>"+ret.name+
+                "</span>,<span>"+ret.fullname+
+                "</span>,<span>"+ret.rw+
+                "</span>,<span>"+ret.valuelist+
+                "</span><br/>";
+            $('#bitsInfo').html(title+desc).css('display', 'block');
         });
     }
 
@@ -188,13 +198,103 @@ function documentCtrl($scope, $http, $interval, user)
 
     // map
 
-    function map() 
+    var moduleRegisters = null;
+
+    function mapShow(reglist) 
     {
         var width = $scope.chip.width;
 
         $scope.bitindex = new Array(width);
         for (var i=0; i<width; i++) $scope.bitindex[i] = i;
 
+        $('#map').css('width', 120+101*reglist.length);
+            
+        /* 重新构建位组序列，需求是：
+         * 1. 同一位组的连续位，合并后重新计算行高
+         * 
+         * 操作步骤
+         * 1. 生成数组： [0-width]:{'id':bits.id, 'cnt':1}
+         * 2. 倒序遍历， 如果当前元素和下一个元素的id相同，则将下一个元素的cnt设置为当前元素的cnt+下一个元素的cnt， 并且将当前元素的cnt清零
+         * 3. 顺序变量数组，取出cnt不为0的元素
+         * 4. 按序取出bits对象，并关联cnt数据
+         */
+        for (var i=0; i<reglist.length; i++) {
+            var j;
+
+            var reg = reglist[i];
+
+            var bits = [];
+            for (j=0; j<width; j++) bits[j] = {'id':0, 'cnt':1};
+            // 构建寄存器位与所属位组的关系
+            for (j=0; j<reg.bitlist.length; j++) {
+                var bits2 = reg.bitlist[j];
+                bits2.bitlist.split(',').map((x)=>{
+                    var idx = parseInt(x);
+                    bits[idx]['id'] = bits2.id; //{'id':bits2.id, 'cnt':1};
+                });
+            }
+            
+            // 合并连续的位组
+            for (j=width-2; j>=0; j--) {
+                if (!bits[j] || !bits[j+1] || (bits[j].id!=bits[j+1].id)) continue;
+                bits[j]['cnt']  += bits[j+1]['cnt'];
+                bits[j+1]['cnt'] = 0;
+            }
+            // 取出有效的位组，并关联位组数据
+            var skip = 0;
+            var bitlist = [];
+            for (j=0; j<width; j++) {
+                if (skip && --skip) continue;
+                
+                if (!bits[j] || !bits[j].id) {
+                    if (bits[j].cnt) bitlist.push({'id':0, 'name':'Reserved', 'cnt':bits[j].cnt});
+                } else {
+                    var k = 0;
+                    for (; (k<reg.bitlist.length) && (bits[j].id!=reg.bitlist[k].id); k++) ;
+                    var tmp = angular.copy(reg.bitlist[k]);
+                    tmp['cnt'] = skip = bits[j].cnt;
+                    bitlist.push(tmp);
+                }
+            }
+            reglist[i]['bitlist2'] = bitlist;
+        }
+        $scope.registerlist = reglist;
+
+        window.setTimeout(bitsRefresh, 10);
+    }
+
+    function mapSearch()
+    {
+        if (!$scope.chip) return;
+
+        var str = $scope.str;
+        if (!str) mapShow(moduleRegisters);
+        else {
+            // 搜索寄存器的：name, fullname, 位组的：name, fullname
+            var reglist = [];
+
+            for (var i=0; i<moduleRegisters.length; i++) {
+                var reg = moduleRegisters[i];
+                if ((reg.name.indexOf(str)==-1) && (reg.fullname.indexOf(str)==-1)) {
+                    for (var j=0; j<reg.bitlist2.length; j++) {
+                        var bits = reg.bitlist2[j];
+                        if ((bits.name.indexOf(str)!=-1) || (bits.fullname && (bits.fullname.indexOf(str)!=-1))) 
+                        {
+                            reglist.push(reg);
+                            break;
+                        }
+                    }
+                } else {
+                    reglist.push(reg);
+                }
+            }
+            
+            mapShow(reglist);
+        }
+    }
+
+    function map() 
+    {
         $http
         .get('/chip/register/map/'+$scope.module.id)
         .then((res)=>{
@@ -206,59 +306,9 @@ function documentCtrl($scope, $http, $interval, user)
             });
             // 根据寄存器地址顺序排列
             reglist = reglist.sort(function(a,b){ return a.address-b.address; });
-            
-            /* 重新构建位组序列，需求是：
-             * 1. 同一位组的连续位，合并后重新计算行高
-             * 
-             * 操作步骤
-             * 1. 生成数组： [0-width]:{'id':bits.id, 'cnt':1}
-             * 2. 倒序遍历， 如果当前元素和下一个元素的id相同，则将下一个元素的cnt设置为当前元素的cnt+下一个元素的cnt， 并且将当前元素的cnt清零
-             * 3. 顺序变量数组，取出cnt不为0的元素
-             * 4. 按序取出bits对象，并关联cnt数据
-             */
-            for (var i=0; i<reglist.length; i++) {
-                var j;
+            moduleRegisters = reglist;
 
-                var reg = reglist[i];
-
-                var bits = [];
-                for (j=0; j<width; j++) bits[j] = {'id':0, 'cnt':1};
-                // 构建寄存器位与所属位组的关系
-                for (j=0; j<reg.bitlist.length; j++) {
-                    var bits2 = reg.bitlist[j];
-                    bits2.bitlist.split(',').map((x)=>{
-                        var idx = parseInt(x);
-                        bits[idx]['id'] = bits2.id; //{'id':bits2.id, 'cnt':1};
-                    });
-                }
-                
-                // 合并连续的位组
-                for (j=width-2; j>=0; j--) {
-                    if (!bits[j] || !bits[j+1] || (bits[j].id!=bits[j+1].id)) continue;
-                    bits[j]['cnt']  += bits[j+1]['cnt'];
-                    bits[j+1]['cnt'] = 0;
-                }
-                // 取出有效的位组，并关联位组数据
-                var skip = 0;
-                var bitlist = [];
-                for (j=0; j<width; j++) {
-                    if (skip && --skip) continue;
-                    
-                    if (!bits[j] || !bits[j].id) {
-                        if (bits[j].cnt) bitlist.push({'id':0, 'name':'Reserved', 'cnt':bits[j].cnt});
-                    } else {
-                        var k = 0;
-                        for (; (k<reg.bitlist.length) && (bits[j].id!=reg.bitlist[k].id); k++) ;
-                        var tmp = angular.copy(reg.bitlist[k]);
-                        tmp['cnt'] = skip = bits[j].cnt;
-                        bitlist.push(tmp);
-                    }
-                }
-                reglist[i]['bitlist2'] = bitlist;
-            }
-            $scope.registerlist = reglist;
-
-            window.setTimeout(bitsRefresh, 10);
+            mapShow(moduleRegisters);
         })
     }
 
@@ -278,8 +328,12 @@ function documentCtrl($scope, $http, $interval, user)
             if (errorCheck(res)) return ;
 
             var ret = res.data.message;
+            var title = "<span>"+ret.name+
+                "</span>,<span>"+ret.fullname+
+                "</span>,<span>"+ret.address+
+                "</span><br/>";
             var desc = ret.desc.replace(new RegExp('\n',"gm"),'<br/>');
-            $('#bitsInfo').html(ret.fullname+'<br/>'+desc).css('display', 'block');
+            $('#bitsInfo').html(title+desc).css('display', 'block');
         });
     }
 
